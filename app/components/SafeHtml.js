b383208a1c49548bcb4736d25fdc8790286060d5
watson-yu <watsonyu@gmail.com>
2026-04-27 18:14:37 +0800

    Preserve inline style attributes in admin preview by adding style to DOMPurify ADD_ATTR. Closes #33

--- app/components/SafeHtml.js
+++ app/components/SafeHtml.js
@@ -2,5 +2,5 @@
 import DOMPurify from "isomorphic-dompurify";
 
 export default function SafeHtml({ html, ...props }) {
-  return <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(html || "", { ADD_TAGS: ["style"], ADD_ATTR: ["target"] }) }} {...props} />;
+  return <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(html || "", { ADD_TAGS: ["style"], ADD_ATTR: ["target", "style"] }) }} {...props} />;
 }
